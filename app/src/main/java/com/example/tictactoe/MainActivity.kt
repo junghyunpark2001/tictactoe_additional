6dcbab20d2b492cc7cd5b085749df798c7cb9ea2
moo <lgmoo2002@gmail.com>
2024-09-22 23:53:39 +0900

    과제 1.5

---
+++ app/src/main/java/com/example/tictactoe/MainActivity.kt
@@ -0,0 +1,75 @@
+package com.example.tictactoe
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.activity.viewModels
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+class MainActivity : AppCompatActivity() {
+
+    private val viewModel : MainViewModel by viewModels()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        //lateinit var binding: ActivityMainBinding
+        //계속 Binding 클래스가 없다고 오류가 뜸... gradle에 정상적으로 추가도해주었는데. 일단 없이 진행.
+
+
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        viewModel.box.observe(this){ box ->
+            updateUI(box)
+            viewModel.CheckFinish()}
+
+        viewModel.restart.observe(this){ res ->
+            if (res == 0)
+                findViewById<TextView>(R.id.button).text = "초기화"
+            else
+                findViewById<TextView>(R.id.button).text = "한판 더"
+        }
+        viewModel.player.observe(this){play ->
+            when(play)
+            {
+                0 -> findViewById<TextView>(R.id.order).text = "O의 차례입니다"
+                1 -> findViewById<TextView>(R.id.order).text ="X의 차례입니다"
+                2 -> findViewById<TextView>(R.id.order).text ="게임 오버"
+                3 -> findViewById<TextView>(R.id.order).text ="무승부"
+            }
+        }
+
+        findViewById<TextView>(R.id.box_1).setOnClickListener { viewModel.PressBox(1) } // 클릭하면 pressbox 함수가 실행되어 데이터가 변한다.
+        findViewById<TextView>(R.id.box_2).setOnClickListener { viewModel.PressBox(2) }
+        findViewById<TextView>(R.id.box_3).setOnClickListener { viewModel.PressBox(3) }
+        findViewById<TextView>(R.id.box_4).setOnClickListener { viewModel.PressBox(4) }
+        findViewById<TextView>(R.id.box_5).setOnClickListener { viewModel.PressBox(5) }
+        findViewById<TextView>(R.id.box_6).setOnClickListener { viewModel.PressBox(6) }
+        findViewById<TextView>(R.id.box_7).setOnClickListener { viewModel.PressBox(7) }
+        findViewById<TextView>(R.id.box_8).setOnClickListener { viewModel.PressBox(8) }
+        findViewById<TextView>(R.id.box_9).setOnClickListener { viewModel.PressBox(9) }
+
+        findViewById<TextView>(R.id.button).setOnClickListener {
+            viewModel.restart()
+        } // 클릭하면 restart 함수 실행되어 데이터 초기화.
+    }
+
+    fun updateUI(box : List<String>){
+        findViewById<TextView>(R.id.box_1).text = box[1]
+        findViewById<TextView>(R.id.box_2).text = box[2]
+        findViewById<TextView>(R.id.box_3).text = box[3]
+        findViewById<TextView>(R.id.box_4).text = box[4]
+        findViewById<TextView>(R.id.box_5).text = box[5]
+        findViewById<TextView>(R.id.box_6).text = box[6]
+        findViewById<TextView>(R.id.box_7).text = box[7]
+        findViewById<TextView>(R.id.box_8).text = box[8]
+        findViewById<TextView>(R.id.box_9).text = box[9]
+    }
+}
---
+++ app/src/main/java/com/example/tictactoe/MainViewModel.kt
@@ -0,0 +1,81 @@
+package com.example.tictactoe
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class MainViewModel: ViewModel() {
+    private val _box: MutableLiveData<List<String>> = MutableLiveData(List(10){""})
+    val box: LiveData<List<String>> get() = _box  // 외부에 공개하는 데이터. 여기서는 필요 없나?
+
+    private var _player: MutableLiveData<Int> = MutableLiveData(0) // 0 : O의 차례입니다, 1 : X의 차례입니다, 2 : 게임 오버, 3 : 무승부
+    val player: LiveData<Int> = _player
+    // 얘로도 업데이트 되니까.
+
+    private var _count : Int = 0
+
+    private var _restart : MutableLiveData<Int> = MutableLiveData(0) // 한판 더 버튼 활성화 여부. restart 1 되면 '재시작'으로 활성화. (UI가 바뀜)
+    val restart : LiveData<Int> = _restart
+
+
+    fun PressBox(index: Int){
+        if (_box.value?.get(index)?.isEmpty() == true && (_player.value == 0 || _player.value == 1)){
+            val updatedBox = _box.value?.toMutableList() ?: return
+            updatedBox[index] = if (_player.value == 0) "O" else "X" // 0이면 O를 추가.
+            _box.value = updatedBox
+            // Box 들의 리스트를 계속 바꾸는. 그리고 각 인덱스는 box의 아이디를 의미함.
+
+            if (_restart.value==0&&_player.value == 0)
+                _player.value = 1
+            else if (_restart.value==0&&_player.value == 1)
+                _player.value = 0
+            else if (_restart.value == 2)
+                _player.value = 2
+            else if (_restart.value == 1)
+                _player.value = 3
+
+
+
+            _count = _count + 1
+        }
+    }
+        // Box들을 눌렀을때 일어나는 일. 타일이 채워지고, 플레이어 O, X가 변하고(그에 따라 문구도 변하고), 게임 횟수가 늘어남.
+
+    fun CheckFinish(){
+        if (_box.value?.get(1) == "X" && _box.value?.get(2) == "X" && _box.value?.get(3) == "X" ||
+            _box.value?.get(4) == "X" && _box.value?.get(5) == "X" && _box.value?.get(6) == "X" ||
+            _box.value?.get(7) == "X" && _box.value?.get(8) == "X" && _box.value?.get(9) == "X" ||
+            _box.value?.get(1) == "X" && _box.value?.get(4) == "X" && _box.value?.get(7) == "X" ||
+            _box.value?.get(2) == "X" && _box.value?.get(5) == "X" && _box.value?.get(8) == "X" ||
+            _box.value?.get(3) == "X" && _box.value?.get(6) == "X" && _box.value?.get(9) == "X" ||
+            _box.value?.get(1) == "X" && _box.value?.get(5) == "X" && _box.value?.get(9) == "X" ||
+            _box.value?.get(3) == "X" && _box.value?.get(5) == "X" && _box.value?.get(7) == "X" ||
+
+            _box.value?.get(1) == "O" && _box.value?.get(2) == "O" && _box.value?.get(3) == "O" ||
+            _box.value?.get(4) == "O" && _box.value?.get(5) == "O" && _box.value?.get(6) == "O" ||
+            _box.value?.get(7) == "O" && _box.value?.get(8) == "O" && _box.value?.get(9) == "O" ||
+            _box.value?.get(1) == "O" && _box.value?.get(4) == "O" && _box.value?.get(7) == "O" ||
+            _box.value?.get(2) == "O" && _box.value?.get(5) == "O" && _box.value?.get(8) == "O" ||
+            _box.value?.get(3) == "O" && _box.value?.get(6) == "O" && _box.value?.get(9) == "O" ||
+            _box.value?.get(1) == "O" && _box.value?.get(5) == "O" && _box.value?.get(9) == "O" ||
+            _box.value?.get(3) == "O" && _box.value?.get(5) == "O" && _box.value?.get(7) == "O"
+            ) {
+            _player.value = 2 // 게임 오버
+            _restart.value = 2
+        }
+        else if (_count == 8){
+            _player.value = 3
+            _restart.value = 1
+        }// 다 채워지면 무승부
+    }
+
+    fun restart(){
+        _restart.value = 0
+        _count = 0
+        _player.value = 0
+        _box.value = List(10) { "" }
+    }
+}
+    // 끝났는지 체크할 수 있도록. 항상 실행이 되어야함. 끝나면 "초기화"가 "한판더"로 바뀜.
+
+// 어차피 콜백 ftn으로 할거니 계속해서 불려질듯?
